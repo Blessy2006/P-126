108820c542da6c26daaa547915d8a18019218c64
Blessy2006 <68602797+Blessy2006@users.noreply.github.com>
2021-06-24 13:21:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import PickImage from "./screen/cam"
+
+export default class PickImage extends React.Component{
+  render(){
+    return(
+      <PickImage/>
+    )
+  }
+}
